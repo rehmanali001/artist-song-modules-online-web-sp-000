532904ce242306e10b954ce14b16ccdcea2764ad
Rehman Ali <rehmanali001@gmail.com>
2019-08-14 20:15:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,11 @@ class Song
   def self.all
     @@songs
   end
-
+  
+  def self.find_by_name(name)
+    @@songs.detect {|a| a.name == name}
+  end
+  
   def artist=(artist)
     @artist = artist
   end
