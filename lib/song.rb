5f8f87997255da372ac4dc58de3d5a86da5435fb
Rehman Ali <rehmanali001@gmail.com>
2019-08-14 20:39:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,16 +7,8 @@ class Song
   attr_reader :artist
   @@songs = []
 
-  def initialize
-    self.class.all << self
-  end
-
   def self.all
     @@songs
-  end
-  
-  def self.find_by_name(name)
-    @@songs.detect {|a| a.name == name}
   end
   
   def artist=(artist)
