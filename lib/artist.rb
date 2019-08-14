520cad9031d8053da414902e291c8a710b6615e5
Rehman Ali <rehmanali001@gmail.com>
2019-08-14 20:35:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods
-  include Paramable
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
   @@artists = []
