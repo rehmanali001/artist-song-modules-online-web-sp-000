edeb7b007271138dea07e9132798c864329e3af3
Rehman Ali <rehmanali001@gmail.com>
2019-08-14 20:40:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,8 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
   attr_accessor :name
   attr_reader :songs
   @@artists = []
