ba3d792684241cd255151f0c9840cd633a874afa
Rehman Ali <rehmanali001@gmail.com>
2019-08-14 20:44:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,13 @@
 require 'pry'
-
 class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   extend Findable
   include Paramable
-  
+
   attr_accessor :name
   attr_reader :songs
-  
+
   @@artists = []
 
   def initialize
@@ -20,10 +19,13 @@ class Artist
     @@artists
   end
 
-  
   def add_song(song)
     @songs << song
     song.artist = self
+  end
+
+  def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
+
 end
